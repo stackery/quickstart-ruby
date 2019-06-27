d97100dda099b0305a87678e059348c959a897ca
Anna Spysz <anna.spysz@stackery.io>
2019-06-26 18:02:49 -0700

    Added function code

--- src/getItems/function.rb
+++ src/getItems/function.rb
@@ -1,5 +1,32 @@
-require 'json'
+require "aws-sdk"
 
 def handler(event:, context:)
-    { event: JSON.generate(event), context: JSON.generate(context.inspect) }
+  dynamodb = Aws::DynamoDB::Client.new
+  table_name = ENV["TABLE_NAME"] # get the table name from the automatically populated environment variables
+
+  begin
+    # Use dynamodb to get items from the ItemTable
+    result = dynamodb.scan({
+      table_name: table_name, 
+    })
+    result_count = result[:scanned_count]
+
+    for item in result[:items] do
+      item_id = item["id"]
+      content = item["content"]
+      puts "Item #{item_id}: #{content}"
+    end
+
+  rescue  Aws::DynamoDB::Errors::ServiceError => error # stop execution if dynamodb is not available
+    puts "Error fetching table #{table_name}. Make sure this function is running in the same environment as the table."
+    puts "#{error.message}"
+  end
+
+  # Return a 200 response if no errors
+  response = {
+    "statusCode": 200,
+    "body": "#{result_count} items found"
+  }
+
+  return response
 end
--- src/postItem/function.rb
+++ src/postItem/function.rb
@@ -1,5 +1,35 @@
-require 'json'
+require "aws-sdk"
 
 def handler(event:, context:)
-    { event: JSON.generate(event), context: JSON.generate(context.inspect) }
+  dynamodb = Aws::DynamoDB::Client.new
+  table_name = ENV["TABLE_NAME"] # get the table name from the automatically populated environment variables
+
+  params = {
+    table_name: table_name,
+    item: {
+      "id" => "5", # modify with each invoke so the id does not repeat
+      "content" => "So much content" # modify content here
+    }, 
+    return_consumed_capacity: "TOTAL",
+    condition_expression: "attribute_not_exists(id)" # do not overwrite existing entries
+  }
+
+  begin
+    # Write a new item to the ItemTable
+    dynamodb.put_item(params)
+    item_id = params[:item]["id"]
+    puts "Writing item #{item_id} to table #{table_name}."
+
+  rescue  Aws::DynamoDB::Errors::ServiceError => error # stop execution if dynamodb is not available
+    puts "Error writing to table #{table_name}. Make sure this function is running in the same environment as the table."
+    puts "#{error.message}"
+  end
+
+  # Return a 200 response if no errors
+  response = {
+    "statusCode": 200,
+    "body": "Success!"
+  }
+
+  return response
 end
